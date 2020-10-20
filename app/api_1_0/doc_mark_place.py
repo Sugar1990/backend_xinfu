5a4ac37cdc4ccb69b638782f4a1c1141cc7dd21a
Sugar1990 <chao_chao1990@126.com>
2020-10-20 11:23:22 +0800

    添加和修改标注地点，dms等参数格式判断

--- app/api_1_0/doc_mark_place.py
+++ app/api_1_0/doc_mark_place.py
@@ -171,25 +171,30 @@ def add_doc_mark_place():
         create_by = request.json.get('create_by', 0)
         entity_or_sys = request.json.get('entity_or_sys', 0)
         appear_index_in_text = request.json.get('appear_index_in_text', [])
-        doc_mark_place = DocMarkPlace.query.filter_by(doc_id=doc_id, word=word, type=type, place_id=place_id,
-                                                      direction=direction, place_lon=place_lon, place_lat=place_lat,
-                                                      height=height,
-                                                      unit=unit, dms=dms, distance=distance, relation=relation,
-                                                      create_by=create_by,
-                                                      entity_or_sys=entity_or_sys,
-                                                      appear_index_in_text=appear_index_in_text, valid=1).first()
-        if doc_mark_place:
-            res = fail_res(msg="文档标记地点已存在!")
+        if not isinstance(dms, list):
+            res = fail_res(msg="dms参数格式错误!")
+        if not isinstance(appear_index_in_text, list):
+            res = fail_res(msg="appear_index_in_text参数格式错误!")
         else:
-            docMarkPlace = DocMarkPlace(doc_id=doc_id, word=word, type=type, place_id=place_id,
-                                        direction=direction, place_lon=place_lon, place_lat=place_lat, height=height,
-                                        unit=unit, dms=dms, distance=distance, relation=relation, create_by=create_by,
-                                        create_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                        appear_index_in_text=appear_index_in_text,
-                                        entity_or_sys=entity_or_sys, valid=1)
-            db.session.add(docMarkPlace)
-            db.session.commit()
-            res = success_res(data={"id": docMarkPlace.id})
+            doc_mark_place = DocMarkPlace.query.filter_by(doc_id=doc_id, word=word, type=type, place_id=place_id,
+                                                          direction=direction, place_lon=place_lon, place_lat=place_lat,
+                                                          height=height,
+                                                          unit=unit, dms=dms, distance=distance, relation=relation,
+                                                          create_by=create_by,
+                                                          entity_or_sys=entity_or_sys,
+                                                          appear_index_in_text=appear_index_in_text, valid=1).first()
+            if doc_mark_place:
+                res = fail_res(msg="文档标记地点已存在!")
+            else:
+                docMarkPlace = DocMarkPlace(doc_id=doc_id, word=word, type=type, place_id=place_id,
+                                            direction=direction, place_lon=place_lon, place_lat=place_lat, height=height,
+                                            unit=unit, dms=dms, distance=distance, relation=relation, create_by=create_by,
+                                            create_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                            appear_index_in_text=appear_index_in_text,
+                                            entity_or_sys=entity_or_sys, valid=1)
+                db.session.add(docMarkPlace)
+                db.session.commit()
+                res = success_res(data={"id": docMarkPlace.id})
     except Exception as e:
         print(str(e))
         db.session.rollback()
@@ -219,58 +224,63 @@ def modify_doc_mark_place():
         create_by = request.json.get('create_by', 0)
         create_time = request.json.get('create_time', None)
         appear_index_in_text = request.json.get('appear_index_in_text', [])
-        doc_mark_place = DocMarkPlace.query.filter_by(id=id, valid=1).first()
-        if doc_mark_place:
-            doc_mark_place1 = DocMarkPlace.query.filter_by(doc_id=doc_id, word=word, type=type, place_id=place_id,
-                                                           direction=direction, place_lon=place_lon,
-                                                           place_lat=place_lat, height=height,
-                                                           unit=unit, dms=dms, distance=distance, relation=relation,
-                                                           create_by=create_by,
-                                                           entity_or_sys=entity_or_sys,
-                                                           appear_index_in_text=appear_index_in_text, valid=1).first()
-            if doc_mark_place1:
-                res = fail_res(msg="文档标记地点已存在")
-            else:
-                if doc_id:
-                    doc_mark_place.doc_id = doc_id
-                if word:
-                    doc_mark_place.word = word
-                if type:
-                    doc_mark_place.type = type
-                if place_id:
-                    doc_mark_place.place_id = place_id
-                if direction:
-                    doc_mark_place.direction = direction
-                if place_lon:
-                    doc_mark_place.place_lon = place_lon
-                if place_lat:
-                    doc_mark_place.place_lat = place_lat
-                if height:
-                    doc_mark_place.height = height
-                if unit:
-                    doc_mark_place.unit = unit
-                if dms:
-                    doc_mark_place.dms = dms
-                if distance:
-                    doc_mark_place.distance = distance
-                if relation:
-                    doc_mark_place.relation = relation
-                if create_by:
-                    doc_mark_place.create_by = create_by
-                if create_time:
-                    doc_mark_place.create_time = create_time
-                if update_by:
-                    doc_mark_place.update_by = update_by
-                # if update_time:
-                doc_mark_place.update_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                if entity_or_sys:
-                    doc_mark_place.entity_or_sys = entity_or_sys
-                if appear_index_in_text:
-                    doc_mark_place.appear_index_in_text = appear_index_in_text
-                db.session.commit()
-                res = success_res()
+        if not isinstance(dms, list):
+            res = fail_res(msg="dms参数格式错误!")
+        if not isinstance(appear_index_in_text, list):
+            res = fail_res(msg="appear_index_in_text参数格式错误!")
         else:
-            res = fail_res(msg="文档标记地点id不存在!")
+            doc_mark_place = DocMarkPlace.query.filter_by(id=id, valid=1).first()
+            if doc_mark_place:
+                doc_mark_place1 = DocMarkPlace.query.filter_by(doc_id=doc_id, word=word, type=type, place_id=place_id,
+                                                               direction=direction, place_lon=place_lon,
+                                                               place_lat=place_lat, height=height,
+                                                               unit=unit, dms=dms, distance=distance, relation=relation,
+                                                               create_by=create_by,
+                                                               entity_or_sys=entity_or_sys,
+                                                               appear_index_in_text=appear_index_in_text, valid=1).first()
+                if doc_mark_place1:
+                    res = fail_res(msg="文档标记地点已存在")
+                else:
+                    if doc_id:
+                        doc_mark_place.doc_id = doc_id
+                    if word:
+                        doc_mark_place.word = word
+                    if type:
+                        doc_mark_place.type = type
+                    if place_id:
+                        doc_mark_place.place_id = place_id
+                    if direction:
+                        doc_mark_place.direction = direction
+                    if place_lon:
+                        doc_mark_place.place_lon = place_lon
+                    if place_lat:
+                        doc_mark_place.place_lat = place_lat
+                    if height:
+                        doc_mark_place.height = height
+                    if unit:
+                        doc_mark_place.unit = unit
+                    if dms:
+                        doc_mark_place.dms = dms
+                    if distance:
+                        doc_mark_place.distance = distance
+                    if relation:
+                        doc_mark_place.relation = relation
+                    if create_by:
+                        doc_mark_place.create_by = create_by
+                    if create_time:
+                        doc_mark_place.create_time = create_time
+                    if update_by:
+                        doc_mark_place.update_by = update_by
+                    # if update_time:
+                    doc_mark_place.update_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    if entity_or_sys:
+                        doc_mark_place.entity_or_sys = entity_or_sys
+                    if appear_index_in_text:
+                        doc_mark_place.appear_index_in_text = appear_index_in_text
+                    db.session.commit()
+                    res = success_res()
+            else:
+                res = fail_res(msg="文档标记地点id不存在!")
     except Exception as e:
         print(str(e))
         db.session.rollback()
