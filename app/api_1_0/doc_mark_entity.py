e96ee504e5b8440b9c30e5a4668df9171003e963
Sugar1990 <chao_chao1990@126.com>
2020-10-20 09:03:08 +0800

    删除paragraph_index和appear_text字段

--- app/api_1_0/doc_mark_entity.py
+++ app/api_1_0/doc_mark_entity.py
@@ -8,7 +8,7 @@ from .. import db
 from .utils import success_res, fail_res
 import time
 
-# doc_mark_entity表中删除了entity_type、entity_type_id字段，添加了paragraph_index字段
+# doc_mark_entity表中删除了entity_type、entity_type_id字段，添加了appear_index_in_text字段
 
 # 按id查询
 @blue_print.route('/get_doc_mark_entity_by_id', methods=['GET'])
@@ -17,7 +17,7 @@ def get_doc_mark_entity_by_id():
         id = request.args.get("id", 0, type=int)
         doc_mark_entity = DocMarkEntity.query.filter_by(id=id, valid=1).first()
         if doc_mark_entity:
-            res = success_res({
+            res = success_res(data={
                 "id": doc_mark_entity.id,
                 "doc_id": doc_mark_entity.doc_id,
                 "word": doc_mark_entity.word,
@@ -34,7 +34,7 @@ def get_doc_mark_entity_by_id():
 
     except Exception as e:
         print(str(e))
-        res = fail_res({
+        res = fail_res(data={
             "id": -1,
             "doc_id": -1,
             "word": "",
@@ -56,21 +56,19 @@ def get_doc_mark_entity_by_doc_id():
     try:
         doc_id = request.args.get("doc_id", 0, type=int)
         doc_mark_entity_list = DocMarkEntity.query.filter_by(doc_id=doc_id, valid=1).all()
-        if doc_mark_entity_list:
-            res = success_res(data=[{
-                "id": i.id,
-                "doc_id": i.doc_id,
-                "word": i.word,
-                "entity_id": i.entity_id,
-                "source": i.source,
-                "create_by": i.create_by,
-                "create_time": i.create_time.strftime("%Y--%m--%d %H:%M:%S") if i.create_time else None,
-                "update_by": i.create_by,
-                "update_time": i.update_time.strftime("%Y--%m--%d %H:%M:%S") if i.update_time else None,
-                "appear_index_in_text": i.appear_index_in_text
-            } for i in doc_mark_entity_list])
-        else:
-            res = fail_res(msg="实体数据不存在")
+
+        res = success_res(data=[{
+            "id": i.id,
+            "doc_id": i.doc_id,
+            "word": i.word,
+            "entity_id": i.entity_id,
+            "source": i.source,
+            "create_by": i.create_by,
+            "create_time": i.create_time.strftime("%Y--%m--%d %H:%M:%S") if i.create_time else None,
+            "update_by": i.create_by,
+            "update_time": i.update_time.strftime("%Y--%m--%d %H:%M:%S") if i.update_time else None,
+            "appear_index_in_text": i.appear_index_in_text
+        } for i in doc_mark_entity_list])
 
     except Exception as e:
         print(str(e))
@@ -107,7 +105,6 @@ def add_doc_mark_entity():
                 doc_mark_entity = DocMarkEntity(doc_id=doc_id, word=word, entity_id=entity_id, source=source,
                                                 create_by=create_by, create_time=create_time,
                                                 update_by=update_by, update_time=update_time,
-                                                paragraph_index=paragraph_index, appear_text=appear_text,
                                                 appear_index_in_text=appear_index_in_text, valid=1)
                 db.session.add(doc_mark_entity)
                 db.session.commit()
@@ -155,8 +152,6 @@ def modify_doc_mark_entity():
                         doc_mark_entity.word = word
                     if entity_id:
                         doc_mark_entity.entity_id = entity_id
-                    if paragraph_index:
-                        doc_mark_entity.entity_type_id = paragraph_index
                     if source:
                         doc_mark_entity.source = source
                     if create_by:
@@ -167,10 +162,6 @@ def modify_doc_mark_entity():
                         doc_mark_entity.updater = update_by
                     if update_time:
                         doc_mark_entity.update_time = update_time
-                    if paragraph_index:
-                        doc_mark_entity.paragraph_index = paragraph_index
-                    if appear_text:
-                        doc_mark_entity.appear_text = appear_text
                     if appear_index_in_text:
                         doc_mark_entity.appear_index_in_text = appear_index_in_text
                     db.session.commit()
