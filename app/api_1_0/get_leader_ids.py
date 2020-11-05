fb2895a275e40742cc69b0a25248266653d4f818
Scandium627 <360134299@qq.com>
2020-11-05 16:58:17 +0800

    增加id字段

--- app/api_1_0/get_leader_ids.py
+++ app/api_1_0/get_leader_ids.py
@@ -5,11 +5,11 @@ from app.models import Permission, Customer
 
 def get_leader_ids():
     leader_ids = []
-    permission = Permission.query.filter(Permission.power>=90, Permission.valid==1).all()
+    permission = Permission.query.filter(Permission.power >= 90, Permission.valid == 1).all()
     permission_ids = [i.id for i in permission]
 
     if permission_ids:
-        leaders = Customer.query.filter(Customer.permission_id.in_(permission_ids), Customer.valid==1).all()
+        leaders = Customer.query.filter(Customer.permission_id.in_(permission_ids), Customer.valid == 1).all()
         leader_ids = [i.id for i in leaders]
 
     return leader_ids
