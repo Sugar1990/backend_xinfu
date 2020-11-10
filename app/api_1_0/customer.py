b44679aca739706b14015913ba271dda906f0325
Scandium627 <360134299@qq.com>
2020-11-10 18:48:34 +0800

    上传逻辑

--- app/api_1_0/customer.py
+++ app/api_1_0/customer.py
@@ -115,7 +115,7 @@ def update_customer():
     uuid = request.json.get('customer_uuid', '')
     username = request.json.get('customer_username', '')
     pwd = request.json.get('customer_pwd', '')
-    permission_id = request.json.get('customer_permission_id', 0)
+    permission_id = request.json.get('customer_permission_uuid', 0)
     try:
         customer = db.session.query(Customer).filter(and_(Customer.uuid == uuid, Customer.valid == 1)).first()
         if customer:
