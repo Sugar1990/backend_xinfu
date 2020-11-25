81f294a75dd89b185b0cad278bf53953951ed2a2
Li Sc <chao_chao1990@126.com>
2020-11-25 17:40:42 +0800

    event_point.py

--- app/api_1_0/event_point.py
+++ app/api_1_0/event_point.py
@@ -13,28 +13,30 @@ import uuid
 @blue_print.route('/add_event_points', methods=['POST'])
 def add_event_points():
     try:
-        title_uuid = request.json.get('title_uuid', None)
+        title_uuid = request.json.get('title_uuid', '')
         entity_name = request.json.get('entity_name', '')
         end_time = request.json.get('end_time', '')
-        details = request.json.get('details', None)
-        details = json.loads(details)
+        details = request.json.get('details', [])
+        # details = json.loads(details)
         for detail in details:
-            event_point = EventPoint.query.filter_by(title_uuid=title_uuid, entity_name=entity_name,
+            entity = Entity.query.filter_by(name=entity_name, valid=1).first()
+            if not entity:
+                entity = Entity(uuid=uuid.uuid1(), name=entity_name, category_uuid=title_uuid,
+                                longitude=detail.get("longitude"), latitude=detail.get("latitude"),
+                                create_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                                update_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                                valid=1)
+                db.session.add(entity)
+                db.session.commit()
+            event_point = EventPoint.query.filter_by(title_uuid=title_uuid, entity_uuid=str(entity.uuid),
                                                      end_time=end_time, event_name=detail.get("event_name"),
                                                      event_desc=detail.get("event_desc"), valid=1).first()
             if event_point:
                 pass
             else:
-                entity = Entity.query.filter_by(entity_name=detail.get("entity_name"), valid=1).first()
-                if not entity:
-                    entity = Entity(uuid=uuid.uuid1(), name=detail.get("entity_name"), category_uuid=title_uuid,
-                                    longitude=detail.get("longitude"), latitude=detail.get("latitude"),
-                                    create_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-                                    update_time=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
-                                    create_by_uuid=detail.get("create_by_uuid"),
-                                    update_by_uuid=detail.get("update_by_uuid"), valid=1)
-                    db.session.add(entity)
-                event_point = EventPoint(uuid=uuid.uuid1(), title_uuid=title_uuid, source=detail.get("source"),
+                event_point = EventPoint(uuid=uuid.uuid1(), title_uuid=title_uuid,
+                                         entity_uuid=str(entity.uuid),
+                                         source=detail.get("source"),
                                          event_name=detail.get("event_name"), event_desc=detail.get("event_desc"),
                                          longitude=detail.get("longitude"), latitude=detail.get("latitude"),
                                          event_time=detail.get("event_time"),
